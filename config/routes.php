fe06f753b9195bc91b27935989328a9b05cf7861
Hubert Weber <128645920+Hushini@users.noreply.github.com>
2025-01-13 18:05:54 +0100

    Add files via upload

--- config/routes.php
+++ config/routes.php
@@ -5,4 +5,16 @@ use App\Controller;
 
 $routes = new RouteCollection();
 
+$routes->add('home', new Route('/', ['_controller' => Controller\HomeController::class . '::index']));
+$routes->add('get_user_schedule', new Route('/api/user/schedule', ['_controller' => Controller\UserScheduleController::class . '::getUserSchedule']));
+$routes->add('scrape_data', new Route('/api/scrape', ['_controller' => Controller\ScraperController::class . '::scrapeData']));
+$routes->add('get_suggestions', new Route('/api/suggestions/{kind}', ['_controller' => Controller\ApiController::class . '::getSuggestions']));
+// new router
+$routes->add('run_scrape', new Route('/api/run-scrape', ['_controller' => Controller\ScraperController::class . '::runScraper']));
+// New routes for CRUD operations on entities
+$routes->add('create_entity', new Route('/api/entity/{entity}', ['_controller' => Controller\EntityController::class . '::createEntity'], methods: ['POST']));
+$routes->add('get_entity', new Route('/api/entity/{entity}/{id}', ['_controller' => Controller\EntityController::class . '::getEntity'], methods: ['GET']));
+$routes->add('update_entity', new Route('/api/entity/{entity}/{id}', ['_controller' => Controller\EntityController::class . '::updateEntity'], methods: ['PUT']));
+$routes->add('delete_entity', new Route('/api/entity/{entity}/{id}', ['_controller' => Controller\EntityController::class . '::deleteEntity'], methods: ['DELETE']));
+
 return $routes;
