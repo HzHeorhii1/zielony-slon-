c1e05c9a53228d2c1068919df88cb2e85ddfef93
Hubert Weber <128645920+Hushini@users.noreply.github.com>
2025-01-13 18:07:05 +0100

    Add files via upload

--- src/Controller/EntityController.php
+++ src/Controller/EntityController.php
@@ -1,1 +1,144 @@
-// ...
+<?php
+
+namespace App\Controller;
+
+//use App\Utils\Container;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class EntityController
+{
+    private EntityManagerInterface $entityManager;
+    private SerializerInterface $serializer;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $encoders = [new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        $this->serializer = new Serializer($normalizers, $encoders);
+    }
+
+    public function createEntity(Request $request, string $entity): JsonResponse
+    {
+        $entityClass = $this->getEntityClass($entity);
+
+        if (!$entityClass) {
+            return new JsonResponse(["error" => "Invalid entity type."], 400);
+        }
+
+        try {
+            $data = json_decode($request->getContent(), true);
+            if(json_last_error() !== JSON_ERROR_NONE || !$data){
+                return new JsonResponse(['error'=>'Invalid json'], 400);
+            }
+
+            $newEntity = $this->serializer->denormalize($data, $entityClass);
+
+            $this->entityManager->persist($newEntity);
+            $this->entityManager->flush();
+
+            $jsonContent =  $this->serializer->serialize($newEntity,'json');
+
+            return new JsonResponse([
+                'message' =>'Entity created succesfully',
+                'data'=> json_decode($jsonContent,true)
+            ],201);
+
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' =>'Failed to create an entity '. $e->getMessage()],500);
+        }
+    }
+
+    public function getEntity(string $entity, int $id): JsonResponse
+    {
+        $entityClass = $this->getEntityClass($entity);
+        if (!$entityClass) {
+            return new JsonResponse(["error" => "Invalid entity type."], 400);
+        }
+        $repository = $this->entityManager->getRepository($entityClass);
+        $entity = $repository->find($id);
+
+        if (!$entity) {
+            return new JsonResponse(["error" => "Entity not found"], 404);
+        }
+
+        $jsonContent =  $this->serializer->serialize($entity,'json');
+        return new JsonResponse(json_decode($jsonContent,true),200);
+    }
+
+    public function updateEntity(Request $request, string $entity, int $id): JsonResponse
+    {
+        $entityClass = $this->getEntityClass($entity);
+
+        if (!$entityClass) {
+            return new JsonResponse(["error" => "Invalid entity type."], 400);
+        }
+
+        $repository = $this->entityManager->getRepository($entityClass);
+        $entityToUpdate = $repository->find($id);
+
+        if (!$entityToUpdate) {
+            return new JsonResponse(["error" => "Entity not found"], 404);
+        }
+        try {
+            $data = json_decode($request->getContent(), true);
+            if(json_last_error() !== JSON_ERROR_NONE || !$data){
+                return new JsonResponse(['error'=>'Invalid json'], 400);
+            }
+            $updatedEntity = $this->serializer->denormalize($data,$entityClass, null, ['object_to_populate'=>$entityToUpdate]);
+            $this->entityManager->persist($updatedEntity);
+            $this->entityManager->flush();
+
+            $jsonContent =  $this->serializer->serialize($updatedEntity,'json');
+            return new JsonResponse([
+                'message'=>'Entity updated succesfully',
+                'data' =>json_decode($jsonContent,true)
+
+            ]);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' =>'Failed to update entity '. $e->getMessage()],500);
+        }
+
+    }
+
+    public function deleteEntity(string $entity, int $id): JsonResponse
+    {
+        $entityClass = $this->getEntityClass($entity);
+        if (!$entityClass) {
+            return new JsonResponse(["error" => "Invalid entity type."], 400);
+        }
+        $repository = $this->entityManager->getRepository($entityClass);
+        $entityToDelete = $repository->find($id);
+        if (!$entityToDelete) {
+            return new JsonResponse(["error" => "Entity not found"], 404);
+        }
+        try {
+            $this->entityManager->remove($entityToDelete);
+            $this->entityManager->flush();
+
+            return  new JsonResponse(['message' => 'Entity deleted'], 200);
+
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Failed to delete an entity ' . $e->getMessage()], 500);
+        }
+    }
+
+    private function getEntityClass(string $entity): ?string
+    {
+        $entityClassMap = [
+            'group' => 'App\Entity\Group',
+            'room' => 'App\Entity\Room',
+            'schedule' => 'App\Entity\Schedule',
+            'subject' => 'App\Entity\Subject',
+            'user' => 'App\Entity\User',
+            'worker' => 'App\Entity\Worker',
+        ];
+        return $entityClassMap[$entity] ?? null;
+    }
+}
