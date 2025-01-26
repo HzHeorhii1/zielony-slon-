5d235a4d08729701417400aac2cb15094d6fb87f
hhuziuk <huziukwork@gmail.com>
2025-01-26 15:07:51 +0100

    deleted symfony from index.php

--- public/index.php
+++ public/index.php
@@ -1,21 +1,25 @@
 <?php
-
-require_once __DIR__ . "/../vendor/autoload.php";
+require_once __DIR__ . "/../bootstrap.php";
 
 use App\Utils\Container;
 use App\Utils\EntityManagerCreator;
-use Dotenv\Dotenv;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\HttpKernel\HttpKernel;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\RequestContext;
+use App\Utils\Request;
+use App\Utils\ControllerResolver;
+use App\Utils\HttpKernel;
+use App\Utils\UrlMatcher;
+use App\Utils\RequestContext;
 use App\Service\ScheduleCleanupService;
+use App\Utils\JsonResponse;
 
 // Load environment variables
-$dotenv = Dotenv::createImmutable(__DIR__ . "/../");
-$dotenv->load();
+$dotenvPath = __DIR__ . DIRECTORY_SEPARATOR . '../.env';
+if (file_exists($dotenvPath)) {
+    $env = parse_ini_file($dotenvPath);
+    foreach ($env as $key => $value) {
+        $_ENV[$key] = $value;
+        $_SERVER[$key] = $value;
+    }
+}
 
 // Create Request object
 $request = Request::createFromGlobals();
@@ -26,30 +30,20 @@ $routes = include __DIR__ . "/../config/routes.php";
 // Context
 $context = new RequestContext();
 $context->fromRequest($request);
-
 // Url Matcher
 $matcher = new UrlMatcher($routes, $context);
-
-// Event Dispatcher
-$dispatcher = new EventDispatcher();
-
 // Entity Manager
 $entityManager = EntityManagerCreator::createEntityManager();
-
 // Create Container
 $container = new Container($entityManager);
 // Create cleanup service
-$scheduleCleanupService = $container->get(ScheduleCleanupService::class); // Отримання сервісу
-
+$scheduleCleanupService = $container->get(ScheduleCleanupService::class);
 // Controller Resolver
 $controllerResolver = new ControllerResolver();
-
-
 // HttpKernel
-$kernel = new HttpKernel($dispatcher, $controllerResolver);
+$kernel = new HttpKernel($controllerResolver);
 
-
-// Schedule cleanup logic
+// schedule cleanup logic
 $currentDate = new \DateTime();
 $summerSemesterEndDate = new \DateTime('2024-07-20');
 $winterSemesterEndDate = new \DateTime('2024-02-15');
@@ -59,24 +53,25 @@ if ($currentDate->format('m-d') === $summerSemesterEndDate->format('m-d') || $cu
 
 try {
     $parameters = $matcher->match($request->getPathInfo());
-    $request->attributes->add($parameters);
+    $request->add($parameters);
     $controller = $parameters["_controller"];
-    if (is_string($controller)) {
-        $controllerParts = explode("::", $controller);
-        $controllerName = $controllerParts[0];
-        $methodName = $controllerParts[1] ?? "__invoke";
+    if (is_array($controller)) {
+        $controllerName = $controller[0];
+        $methodName = $controller[1] ?? "__invoke";
         $controllerInstance = $container->get($controllerName);
         $controller = [$controllerInstance, $methodName];
     }
-    $request->attributes->set("_controller", $controller);
+
+    $request->attributes["_controller"] = $controller;
     $response = $kernel->handle($request);
-} catch (\Symfony\Component\Routing\Exception\ResourceNotFoundException $e) {
-    $response = new \Symfony\Component\HttpFoundation\JsonResponse(
+
+} catch (\App\Utils\ResourceNotFoundException $e) {
+    $response = new JsonResponse(
         ["error" => "Not Found"],
         404
     );
 } catch (\Exception $e) {
-    $response = new \Symfony\Component\HttpFoundation\JsonResponse(
+    $response = new JsonResponse(
         ["error" => "Internal Server Error: " . $e->getMessage()],
         500
     );
